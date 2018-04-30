c0b55c5785f7b8c3fd659813184a6341e17dc414
Tim <timothy.mace@outlook.com>
2018-04-30 18:28:25 -0400

    rel path fixed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,15 @@
-const path = require("path");
-
+const path = require("path"),
+  express = require("express");
 
 module.exports = app => {
+  app.use(express.static(path.join(__dirname, "../public/")));
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.sendFile(path.join(__dirname, "/", "../public/home.html"));
   });
 
   app.get("/survey", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
+    res.sendFile(path.join(__dirname, "/", "../public/survey.html"));
   });
 };
